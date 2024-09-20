9390da3da5bb4c105f94fe34de2e7414c6350378
Šimun <simun.hordov5@gmail.com>
2024-09-20 19:26:14 +0200

    Wrote styles for header and site-title which adapted for small.middle,and big screen

--- to-do-list-app/src/App.jsx
+++ to-do-list-app/src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
-
 import "./App.css";
 
-function App() {}
-
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1 id="site-title">To Do List With Prioritization</h1>
+        <button> Add Task</button>
+      </div>
+    </div>
+  );
+}
 export default App;
