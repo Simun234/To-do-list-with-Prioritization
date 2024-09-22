087f7eff0caf916ab7d4c91b8ce94b5d3a96a3c8
Šimun <simun.hordov5@gmail.com>
2024-09-22 09:04:44 +0200

    Added under the header dropdown and in css adapted for small middle and big screen

--- to-do-list-app/src/App.jsx
+++ to-do-list-app/src/App.jsx
@@ -8,6 +8,35 @@ function App() {
         <h1 id="site-title">To Do List With Prioritization</h1>
         <button id="add-btn"> Add Task</button>
       </div>
+      <div className="dropdown">
+        <input
+          type="text"
+          placeholder="Search.."
+          id="myInput"
+          onkeyup="filterFunction()"
+        />
+        <p className="filter">Filter By:</p>
+        <ul className="filter-list">
+          <li>
+            <button onClick="filterTasks('all')">All Tasks</button>
+          </li>
+          <li>
+            <button onClick="filterTasks('high')">High Priority</button>
+          </li>
+          <li>
+            <button onClick="filterTasks('medium')">Medium Priority</button>
+          </li>
+          <li>
+            <button onClick="filterTasks('low')">Low Priority</button>
+          </li>
+          <li>
+            <button onClick="filterTasks('completed')">Completed</button>
+          </li>
+          <li>
+            <button onClick="filterTasks('pending')">Pending</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
