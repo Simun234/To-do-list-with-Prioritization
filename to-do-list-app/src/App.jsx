c6639649e1d33ee362e4f143877693f0553dc360
Šimun <simun.hordov5@gmail.com>
2024-09-22 09:44:22 +0200

    Under Dropdown added table where put task,due date,priority,status and action and in css adapted table and letters for small middle and big screen

--- to-do-list-app/src/App.jsx
+++ to-do-list-app/src/App.jsx
@@ -37,6 +37,93 @@ function App() {
           </li>
         </ul>
       </div>
+      <div class="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>TASK</th>
+              <th>Due Date</th>
+              <th>Priority</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Buy Groceries</td>
+              <td>October 5</td>
+              <td>Medium</td>
+              <td>Pending</td>
+              <td>
+                <button>Edit/Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Pay Utility Bills</td>
+              <td>October 10</td>
+              <td>High</td>
+              <td>In Progress</td>
+              <td>
+                <button>Edit/Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Book Doctor Appointment</td>
+              <td>October 15</td>
+              <td>Medium</td>
+              <td>Completed</td>
+              <td>
+                <button>Edit/Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Exercise (Gym)</td>
+              <td>October 20</td>
+              <td>Low</td>
+              <td>In Progress</td>
+              <td>
+                <button>Edit/Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Call Mom</td>
+              <td>October 22</td>
+              <td>High</td>
+              <td>Pending</td>
+              <td>
+                <button>Edit/Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Clean the House</td>
+              <td>October 24</td>
+              <td>Low</td>
+              <td>In Progress</td>
+              <td>
+                <button>Edit/Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Plan Weekend Trip</td>
+              <td>October 26</td>
+              <td>Medium</td>
+              <td>Pending</td>
+              <td>
+                <button>Edit/Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Read 50 Pages of Book</td>
+              <td>October 30</td>
+              <td>Low</td>
+              <td>In Progress</td>
+              <td>
+                <button>Edit/Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
